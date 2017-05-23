49e7336b74123ab99de76b36b6562625ea4ba222
Joshua <cumtyjh@163.com>
2017-05-23 15:00:23 +0800

    add common util

--- apier-security/src/main/java/cn/apier/security/ApierSecurityApplication.java
+++ apier-security/src/main/java/cn/apier/security/ApierSecurityApplication.java
@@ -1,8 +1,16 @@
-package PACKAGE_NAME;
+package cn.apier.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by yanjunhua on 2017/5/23.
  */
+@SpringBootApplication
 public class ApierSecurityApplication
 {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ApierSecurityApplication.class, args);
+    }
 }
